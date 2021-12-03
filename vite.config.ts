170eb49a35360bd571bec2471a768cb8d79d6142
kaicheng <kaicheng@leju.com>
2021-12-03 16:49:40 +0800

    ci: 测试提交流程

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 import Components from 'unplugin-vue-components/vite';
 import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
